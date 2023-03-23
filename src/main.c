7dd2dc262a4b5ed7b40a428a5493088f04749191
thuantm <minhthuan1091996@gmail.com>
2023-03-23 08:59:29 +0700

    Reading button with interrupt

--- src/main.c
+++ src/main.c
@@ -6,22 +6,29 @@
  * Tested on nRF Connect SDK Version : 2.0
  */
 
-#include <zephyr.h>
+#include <zephyr/kernel.h>
 #include <drivers/gpio.h>
 
-/* STEP 7 - Change the sleep time from 1000 ms to 100 ms */
-#define SLEEP_TIME_MS   1000
+/* STEP 9 - Increase the sleep time from 100ms to 10 minutes  */
+#define SLEEP_TIME_MS   100
 
-/* STEP 3.1 - Get the node identifier for button 1 through its alias sw0 */
-#define SW1			DT_ALIAS(sw1)
-
-/* STEP 3.2 - Get the device pointer. pin number, and pin's configuration flags through gpio_dt_spec */
-static const struct gpio_dt_spec sw1_dev = GPIO_DT_SPEC_GET(SW1, gpios);
+/* SW0_NODE is the devicetree node identifier for the "sw0" alias */
+#define SW0_NODE	DT_ALIAS(sw0) 
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 
 /* LED0_NODE is the devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE	DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+
+
+/* STEP 4 - Define the callback function */
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    gpio_pin_toggle_dt(&led);
+}
+/* STEP 5 - Define a variable of type static struct gpio_callback */
+static struct gpio_callback pin_cb_data;
 
 void main(void)
 {
@@ -30,8 +37,8 @@ void main(void)
 	if (!device_is_ready(led.port)) {
 		return;
 	}
-	/* STEP 4 - Verify that the device is ready for use */
-	if (!device_is_ready(sw1_dev.port)) {
+
+	if (!device_is_ready(button.port)) {
 		return;
 	}
 
@@ -40,27 +47,26 @@ void main(void)
 		return;
 	}
 
-	/* STEP 5 - Configure the pin connected to the button to be an input pin and set its hardware specifications */
-	ret = gpio_pin_configure_dt(&sw1_dev, GPIO_INPUT);
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret < 0) {
 		return;
 	}
-	gpio_pin_set_dt(&led, 0);
+
+	/* STEP 3 - Configure the interrupt on the button's pin */
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+	/* STEP 6 - Initialize the static struct gpio_callback variable   */
 	gpio_pin_set_dt(&led, 1);
-
-
+	gpio_pin_set_dt(&led, 0);
+    
+	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+	gpio_init_callback(&pin_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &pin_cb_data);
 	while (1) {
-		/* STEP 6.1 - Read the status of the button and store it */
-		bool val = gpio_pin_get_dt(&sw1_dev);
-		/* STEP 6.2 - Update the LED to the status of the button */
-		if(val)
-		{
-			gpio_pin_set_dt(&led, 1);
-		}
-		else 
-		{
-			gpio_pin_set_dt(&led, 0);
-		}
-		// k_msleep(SLEEP_TIME_MS); // Put the main thread to sleep for 100ms for power optimization
+		/* STEP 8 - Remove the polling code */
+
+        k_msleep(SLEEP_TIME_MS); 
 	}
 }
